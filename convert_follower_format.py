18e5d08515cba05bd042ffac416fc320df229bc9
Kevin Goldsmith <kevin@unitcircle.com>
2020-04-04 18:14:17 -0700

    added new sorted by follower model

--- convert_follower_format.py
+++ convert_follower_format.py
@@ -18,6 +18,14 @@ data_path = os.path.join(DATA_DIR, DATA_FILE)
 #     lost_followers: []
 # }, ....]
 
+# Follower list format (key is follower id)
+# {
+#     xxxx: {
+#       follow: [],
+#       unfollow: []
+#     }, ...
+# ]
+
 # CSV Format
 # [{
 #     iso_time: ''
@@ -26,15 +34,30 @@ data_path = os.path.join(DATA_DIR, DATA_FILE)
 #     lost: 0
 # }, ...]
 
+def update_follow_dict(follow_dict, date, id_list, added=True):
+    key = 'follow'
+    if not added:
+        key = 'unfollow'
+    
+    for id in id_list:
+        if id in follow_dict:
+            if key in follow_dict[id]:
+                follow_dict[id][key].append(date)
+            else:
+                follow_dict[id][key] = [date]
+        else:
+            follow_dict[id]={ key: [date] }
 
 
 try:
     with open(data_path, 'r') as f:
         followers_dict = json.load(f)
+        print("loaded: {}".format(data_path))
         followers_over_time = dict_to_ordereddict(followers_dict)
 
         new_follower_list = []
         follower_count_list = []
+        follow_dict = {}
         last_item = None
         count = 0
 
@@ -52,26 +75,36 @@ try:
                 follower_count_item['lost'] = len(lost_ids)
                 if len(new_ids) > 0:
                     follower_item['new_followers'] = new_ids
+                    update_follow_dict(follow_dict, item[0], new_ids)
                 if len(lost_ids) > 0:
                     follower_item['lost_follers'] = lost_ids
+                    update_follow_dict(follow_dict, item[0], lost_ids, False)
             else:
                 follower_item['follower_id_list'] = item[1]
                 follower_count_item['added'] = 0
                 follower_count_item['lost'] = 0
+                for id in item[1]:
+                    follow_dict[id]={'follow': [item[0]]}
 
             new_follower_list.append(follower_item)
             follower_count_list.append(follower_count_item)
             last_item = item
         new_follower_list[-1]['follower_id_list'] = last_item[1]
         
+        print("writing: {}".format('new_Foolowers.json'))
         with open(os.path.join(DATA_DIR, 'new_followers.json'), 'w') as f:
             json.dump(new_follower_list, f)
         
+        print("writing: {}".format('new_follower_count.csv'))
         with open(os.path.join(DATA_DIR, 'new_follower_count.csv'), 'w', newline='') as f:
             fieldnames = ['iso_time', 'followers', 'added', 'lost']
             writer = csv.DictWriter(f, fieldnames=fieldnames, quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
             writer.writeheader()
             writer.writerows(follower_count_list)
+        
+        print("writing: {}".format('by_followers.json'))
+        with open(os.path.join(DATA_DIR, 'by_followers.json'), 'w') as f:
+            json.dump(follow_dict, f, indent=2)
 
 except IOError:
     pass
