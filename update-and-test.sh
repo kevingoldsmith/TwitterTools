74f85afadaf35974121db281c290dfbb21f988bf
Kevin Goldsmith <kevin@unitcircle.com>
2020-05-11 22:07:55 -0700

    added filenames to update-and-test, fixed bug (I think) in track_followers_v2 using id as an int instead of string

--- update-and-test.sh
+++ update-and-test.sh
@@ -1,4 +1,8 @@
+echo "track-followers.py" >> testout.log
 (time python track-followers.py) >> testout.log 2>&1
+echo "track-followers_v2.py" >> testout.log
 (time python track_followers_v2.py) >> testout.log 2>&1
+echo "fix_time.py" >> testout.log
 (python fix_time.py) >> testout.log 2>&1
+echo "test_new_followers_format.py" >> testout.log
 (python test_new_followers_format.py) >> testout.log 2>&1
